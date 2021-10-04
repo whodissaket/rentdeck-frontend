b5e28612e691b50dd8508666daf49b22f6d90df6
Saket Nandanwar <70093885+whodissaket@users.noreply.github.com>
2021-10-04 17:21:34 +0530

    Updated:Styling for login and Register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,12 +5,7 @@ import { mobile } from "../responsive";
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  background: linear-gradient(
-      rgba(255, 255, 255, 0.5),
-      rgba(255, 255, 255, 0.5)
-    ),
-    url("https://images.unsplash.com/photo-1449247709967-d4461a6a6103?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2071&q=80")
-      center;
+  background: url("https://i.imgur.com/U50YLC1.png") center;
   background-size: cover;
   display: flex;
   align-items: center;
@@ -21,7 +16,7 @@ const Wrapper = styled.div`
   width: 40%;
   padding: 20px;
   border-radius: 10px;
-  background-color: #fcdec0;
+  background-color: white;
   ${mobile({ width: "75%" })}
 `;
 
@@ -53,12 +48,12 @@ const Button = styled.button`
   border: none;
   padding: 15px 20px;
   border-radius: 5px;
-  background-color: #5e454b;
+  background-color: #3647d9;
   color: white;
   cursor: pointer;
 
   &:hover {
-    background-color: #634e53;
+    background-color: #4f5dd6;
   }
 `;
 const GButton = styled.button`
