aa90b794d52ee312bdbb4d4e75fab1b2677d42f3
mranonymod <mryash018@gmail.com>
2021-10-24 13:07:27 +0530

    Displayed Products in Order

--- src/components/Orders/OrderedProducts.jsx
+++ src/components/Orders/OrderedProducts.jsx
@@ -9,13 +9,17 @@ import {
   CardMedia,
   CardContent,
 } from "@material-ui/core";
+import axios from "axios";
+import { Link } from "react-router-dom";
 import React from "react";
+import Product1 from "./Product1"
 import { useStyles } from "../../pages/styles/style2";
 
 const OrderedProducts = () => {
   const classes = useStyles();
-  const cart = useSelector((state) => state.cart)
-  const { cartItems } = cart
+  const orderDetails = useSelector((state) => state.orderDetails)
+  const { order, loading, error } = orderDetails
+  console.log(order)
   return (
     <div>
       <Container maxWidth="lg">
@@ -24,75 +28,8 @@ const OrderedProducts = () => {
             Products
           </Typography>
           <Grid container spacing={5}>
-            { cartItems?.map((item) => {
-           
-           {/*let isAddReview = false;
-            const reviewExist = item.product.reviews?.find(
-              (review) => review.order == order._id
-            );
-
-            if (order.status === "DELIVERED" && !reviewExist && !admin) {
-              isAddReview = true;
-            } */}
-            return (
-            <>
-            <Grid key="" item xs={12} sm={6} lg={4}>
-              <Card className={classes.product}>
-                <CardActionArea
-                // onClick={() => history.push(`/product/${item.product._id}`)}
-                >
-                  <CardMedia
-                    style={{ height: "250px", backgroundSize: "contain" }}
-                    className={classes.media}
-                    //   image={item.product.image}
-                    //   title={item.product.name}
-                  />
-                </CardActionArea>
-
-                <CardContent>
-                  <Typography
-                    className={classes.productName}
-                    variant="h5"
-                    //   component={Link}
-                    //   to={`/product/${item.product._id}`}
-                  >
-                    Product Name
-                  </Typography>
-                  <Typography>
-                    <span>Quantity</span>Qty
-                  </Typography>
-                  <Typography>
-                    <span>Price</span> Rs.Price
-                  </Typography>
-                  <Typography>
-                    <span>Total Price</span> Rs.Total
-                  </Typography>
-
-                  {/* {!admin && reviewExist ? (
-                  <Button variant="contained" disabled>
-                    Already reviewed!
-                  </Button>
-                ) : null} */}
-                </CardContent>
-
-                {/* {isAddReview && (
-                <CardActions>
-                  <Button
-                    style={{ color: "white" }}
-                    variant="contained"
-                    color="primary"
-                    // onClick={() =>
-                    //   history.push(
-                    //     `/order/${id}/review/${item.product._id}`
-                    //   )
-                    // }
-                  >
-                    Add Review
-                  </Button>
-                </CardActions>
-              )} */}
-              </Card>
-            </Grid></>)})}
+            { order?.orderItems?.map((item) =><Product1 item={item} />
+            )}
           </Grid>
           
 
---
+++ src/components/Orders/Product1.jsx
@@ -0,0 +1,87 @@
+import { useSelector } from "react-redux";
+import {
+  Container,
+  Typography,
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+} from "@material-ui/core";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import React from "react";
+import { useEffect,useState } from "react";
+import { useStyles } from "../../pages/styles/style2";
+const Product1 = ({item}) => {
+    const classes = useStyles();
+    const [product , setProd] = useState(null)
+    useEffect(() => {
+        axios.get(`http://localhost:5000/api/products/id/${item.product}`)
+    .then((response) => { setProd(response.data)
+   console.log(product)})
+.catch((error) => console.log(error.message))
+    }, [item])
+
+     return (
+     <>
+     <Grid key={item?._id} item xs={12} sm={6} lg={4}>
+       <Card className={classes.product}>
+         <CardActionArea
+         // onClick={() => history.push(`/product/${item.product._id}`)}
+         >
+           <CardMedia
+             style={{ height: "250px", backgroundSize: "contain" }}
+             className={classes.media}
+             image={product?.images?.[0]}
+             title={product?.title}
+           />
+         </CardActionArea>
+
+         <CardContent>
+           <Typography
+             className={classes.productName}
+             variant="h5"
+             component={Link}
+             to={`/product/${product?._id}`}
+           >
+             {console.log(product)}
+           </Typography>
+           <Typography>
+             <span>Quantity</span> {item?.qty}
+           </Typography>
+           <Typography>
+             <span>Price</span> {product?.rentalrate}
+           </Typography>
+           <Typography>
+             <span>Total Price</span> Rs.{product?.rentalrate * item?.qty}
+           </Typography>
+
+           {/* {!admin && reviewExist ? (
+           <Button variant="contained" disabled>
+             Already reviewed!
+           </Button>
+         ) : null} */}
+         </CardContent>
+
+         {/* {isAddReview && (
+         <CardActions>
+           <Button
+             style={{ color: "white" }}
+             variant="contained"
+             color="primary"
+             // onClick={() =>
+             //   history.push(
+             //     `/order/${id}/review/${item.product._id}`
+             //   )
+             // }
+           >
+             Add Review
+           </Button>
+         </CardActions>
+       )} */}
+       </Card>
+     </Grid></>)
+}
+
+export default Product1
