257bed08474de5298c3b12d0d57bbdab82552777
Saket Nandanwar <70093885+whodissaket@users.noreply.github.com>
2021-11-10 15:01:03 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,13 @@ import Announcement from "../components/Announcement/Announcement";
 import Categories from "../components/Categories/Categories";
 import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
-
+import styled from "styled-components";
 import Products from "../components/Products/Products";
 import Slider from "../components/Slider/Slider";
+
+const Text = styled.h3`
+  text-align: center;
+`;
 
 const Home = () => {
   return (
@@ -14,6 +18,7 @@ const Home = () => {
       <Navbar />
       <Slider />
       <Categories />
+      <Text>All Products</Text>
       <Products category={null} search={null} />
       <Footer />
     </div>
