6530834e18cc5c215a751780f1e1396386729c17
mranonymod <mryash018@gmail.com>
2022-04-02 11:43:48 +0530

    PWA checks

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store";
 import dotenv from  'dotenv'
+import * as serviceWorker from './serviceWorker';
 dotenv.config()
 ReactDOM.render(
   <React.StrictMode>
@@ -14,3 +15,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+serviceWorker.register(); 
